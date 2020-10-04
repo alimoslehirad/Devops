3b181c888c7531cf512a596f273d270c5921ae72
alimoslehirad <alimoslehirad@gmail.com>
2020-10-04 09:40:57 +0330

    compression add

--- openshift-cluster-backuping/cluster-backup.sh
+++ openshift-cluster-backuping/cluster-backup.sh
@@ -5,16 +5,32 @@ mkdir $(date +"%d-%m-%Y")
 OPENSHIFT_COM=(DeploymentConfig replicationcontroller buildconfig route service )
 for ns in `oc get ns | awk '{print $1}'`  
 do
+	if [ $ns -eq "NAME"  ]
+	then
+		continue
+	fi
+
 	echo start $ns yaml file backauping ...
 	for component_type in ${OPENSHIFT_COM[@]}
 	do	
+		
+		if [ $component_type -eq "NAME" ]
+		then
+			continue
+		fi
 		mkdir -p $(date +"%d-%m-%Y")/$ns/$component_type 
 		echo $component_type ...
 		for components in `oc -n $ns get $component_type | awk '{print $1}'`
 		do
 			#touch $(date +"%d-%m-%Y")/$ns/$component_type/$components.yaml
+			if [ $components -eq "NAME" ]
+			then
+				continue
+			fi
 			oc -n $ns get $component_type $components -o yaml > $(date +"%d-%m-%Y")/$ns/$component_type/$components.yaml
 			echo $components
 		done
 	done
 done
+tar cvfz  "$(date +"%d-%m-%Y")".tar.gz $(date +"%d-%m-%Y")
+rm -rf $(date +"%d-%m-%Y")
